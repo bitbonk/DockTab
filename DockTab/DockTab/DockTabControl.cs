e5cfc34edb77bb05b1f2f26dee4acdc22c8419c0
bitbonk <bitbonk@msn.com>
2010-02-20 18:04:32 +0000

    Created Project

---
+++ DockTab/DockTab/DockTabControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace DockTab
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DockTabControl : Selector
+    {
+        static DockTabControl()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof (DockTabControl),
+            //                                         new FrameworkPropertyMetadata(typeof (DockTabControl)));
+        }
+    }
+}
---
+++ DockTab/DockTab/DockTabItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DockTab
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DockTabItem : HeaderedContentControl
+    {
+        static DockTabItem()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof (DockTabItem),
+                                                     //new FrameworkPropertyMetadata(typeof (DockTabItem)));
+        }
+    }
+}
