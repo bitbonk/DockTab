32600d93b91a66995fa3f48789342d93be81c847
bitbonk <bitbonk@msn.com>
2010-09-14 20:00:53 +0000

    Added Test Solution to experiment with White

---
+++ WhiteTest/WhiteTest/WhiteTest.ApplicationUnderTest/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace WhiteTest.ApplicationUnderTest
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Me was clicked!");
+        }
+    }
+}
---
+++ WhiteTest/WhiteTest/WhiteTest/WindowUnderTest.cs
@@ -0,0 +1,32 @@
+﻿using Repository;
+using White.Core.UIItems;
+using White.Core.UIItems.WindowItems;
+
+/// <summary>
+/// The window under test.
+/// </summary>
+public class WindowUnderTest : AppScreen
+{
+    private Button myButton;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowUnderTest"/> class.
+    /// </summary>
+    /// <param name="window">
+    /// The window.
+    /// </param>
+    /// <param name="screenRepository">
+    /// The screen repository.
+    /// </param>
+    public WindowUnderTest(Window window, ScreenRepository screenRepository)
+        : base(window, screenRepository)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowUnderTest"/> class.
+    /// </summary>
+    protected WindowUnderTest()
+    {
+    }
+}
